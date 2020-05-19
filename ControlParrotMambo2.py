4fd5906c10fe58176b3556f49e0e73d2e8fad880
Enriquegarcia10 <63658641+Enriquegarcia10@users.noreply.github.com>
2020-05-19 19:11:11 +0200

    Add files via upload

---
+++ ControlParrotMambo2.py
@@ -0,0 +1,335 @@
+from tkinter import *
+from pyparrot.Minidrone import Mambo
+import serial
+import time
+import simplejson
+import math
+
+
+####PROGRAMACIÓN DRONE####
+
+
+
+
+MACmambo = "D0:3A:89:B3:E6:5A"              #dirección MAC bluetooth del parrot mambo
+
+mambo = Mambo(MACmambo, use_wifi=False)     #declaración variable del drone
+
+arduino = serial.Serial('/dev/ttyUSB0', 115200)
+
+dead_zone= 4
+
+
+#DEFINICION DE VARIABLES
+
+#angulos relativos
+oldz = 0
+oldy = 0
+oldx = 0
+restaz=0
+restay=0
+restax=0
+
+
+#contador para lectura de la bateria
+
+counter=0
+
+#zonas muertas y maximos para filtrado de angulos
+
+zonamuerta_z = 0.5
+zonamuerta_y = 0.5
+zonamuerta_x = 0.5
+maximo_z = 5
+maximo_y = 5
+maximo_x = 5
+
+
+
+def conectar():
+
+    print("intentando conectar")
+    success = mambo.connect(num_retries=3)
+    print("conectado: %s" % success)
+
+    if (success):
+
+        
+        print("sleeping")
+        mambo.smart_sleep(1)
+        mambo.ask_for_state_update()         # Se coge la información del estado del dron(importante para mostrar el nivel de la bateria)
+        mambo.smart_sleep(1)
+        
+    ventana.update()                         # se actualiza la ventana(si no se congelaria en el boton)
+
+def despegar():
+
+    mambo.smart_sleep(2)
+    mambo.ask_for_state_update()
+    mambo.smart_sleep(2)
+
+    print("despegando")
+    mambo.safe_takeoff(5)
+    ventana.update()
+
+
+def aterrizar():
+
+    print("aterrizando")
+    mambo.safe_land(10)
+    ventana.update()
+
+def derecha():
+
+    mambo.fly_direct(roll=velocidad.get(), pitch=0, yaw=0, vertical_movement=0, duration=0.5)
+    ventana.update()
+
+def abajo():
+
+    mambo.fly_direct(roll=0, pitch=-(velocidad.get()), yaw=0, vertical_movement=0, duration=0.5)
+    ventana.update()
+
+def izquierda():
+
+    mambo.fly_direct(roll=-(velocidad.get()), pitch=0, yaw=0, vertical_movement=0, duration=0.5)
+    ventana.update()
+
+def arriba():
+
+    mambo.fly_direct(roll=0, pitch=velocidad.get(), yaw=0, vertical_movement=0, duration=0.5)
+    ventana.update()
+
+def subir():
+
+    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=velocidad.get(), duration=0.5)
+    ventana.update()
+
+
+def bajar():
+
+    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-(velocidad.get()), duration=0.5)
+    ventana.update()
+
+def rotar_izquierda():
+
+    mambo.turn_degrees(10)
+    ventana.update()
+
+
+def rotar_derecha():
+
+    mambo.turn_degrees(-10)
+    ventana.update()
+
+def desconectar():
+
+    print("desconectando")
+    mambo.disconnect()
+    print("desconectado")
+    ventana.update()
+
+def cargabateria():
+
+    
+    nivelbateria.set(mambo.sensors.battery)
+    ventana.update()
+
+def correcion_angulos(angulo, zona_muerta, maximo ):
+
+    signo=math.copysign(1, angulo)      #se coge el signo del ángulo
+    valor = abs(angulo)                 #se trabaja con valores absolutos(al final se dará signo)
+
+    if (valor < zona_muerta):           #Los cambios de posicion inferiores a la zona muerto no són cogidos
+
+        angulo = 0
+        return angulo
+
+    else:
+        
+            
+        valor = float(min(valor, maximo))  #se coge el mínimo entre el valor entrado y el maximo definido
+            
+        valor -= zona_muerta
+           
+
+    angulo = float(signo*valor)
+    return angulo
+
+
+
+
+####DISEÑO TKINTER#####
+
+
+#características ventana tkinter
+
+ventana = Tk()
+ventana.title("control parrot Mambo")
+ventana.geometry("800x700")
+ventana.configure(background="white")
+
+
+#configuración velocidad
+
+velocidad=Scale(ventana,from_ =0, to=200, orient=HORIZONTAL,length=220, label= "Máxima inclinación (velocidad) " ,bg="white", tickinterval=100)
+velocidad.set(20)                            #Se inicializa como velocidad 20
+velocidad.place(x=20,y=50)
+
+
+
+#imagenes
+
+imagen_drone = PhotoImage(file="parrotmambo.png")
+imagen_bateria = PhotoImage(file="bateria.png")
+imagen_boton7 = PhotoImage(file= "botderecha.png")
+imagen_boton8 = PhotoImage(file= "botderecha2.png")
+imagen_boton9 = PhotoImage(file= "botderecha23.png")
+imagen_boton10 = PhotoImage(file= "botderecha234.png")
+imagen_boton11 = PhotoImage(file= "botbajar.png")
+imagen_boton12 = PhotoImage(file= "botbajar2.png")
+imagen_boton13 = PhotoImage(file= "botrotar.png")
+imagen_boton14 = PhotoImage(file= "botrotar2.png")
+
+#Posición de las imagenes
+
+imagen_drone_posi = Label (ventana,image=imagen_drone). place(x=264,y=20)
+imagen_bateria_posi = Label (ventana, image=imagen_bateria). place(x=660,y= 20)
+
+#Declaracion de los botones
+
+
+boton3 = Button(ventana, text = "EMERGENCIA", width = 10 , height = 1,font= ("Italic" , 15, "bold"), fg="white",bg="red",   command = lambda: click_boton(3) )
+boton4 = Button(ventana, text = "DESPEGAR", width = 10 , height = 1, font= ("Italic" , 10, "bold"), fg="white",bg="grey50", command = despegar )
+boton5 = Button(ventana, text = "ATERRIZAR", width = 10 , height = 1, font= ("Italic" , 10, "bold"), fg="white",bg="grey50", command = aterrizar )
+boton6 = Button(ventana, text = "CONECTAR", width = 8 , height = 1, font= ("Italic" , 10, "bold"), fg="white",bg="green", command = conectar )
+boton7 = Button(ventana, image=imagen_boton7, width = 50 , height = 50, command= derecha )
+boton8 = Button(ventana, image=imagen_boton8, width = 50 , height = 50, command = abajo )
+boton9 = Button(ventana, image=imagen_boton9, width = 50 , height = 50, command = izquierda )
+boton10 = Button(ventana, image=imagen_boton10, width = 50 , height = 50, command = arriba )
+boton11 = Button(ventana, image=imagen_boton11, width = 50 , height = 50, command = bajar )
+boton12 = Button(ventana, image=imagen_boton12, width = 50 , height = 50, command = subir )
+boton13 = Button(ventana, image=imagen_boton13, width = 50 , height = 50, command = rotar_derecha )
+boton14 = Button(ventana, image=imagen_boton14, width = 50 , height = 50, command = rotar_izquierda  )
+boton15 = Button(ventana, text = "DESCONECTAR", width = 10 , height = 1, font= ("Italic" , 10, "bold"), fg="white",bg="blue2", command = desconectar )
+
+#Posicionamiento de los botones
+
+
+boton3.place(x=600,y=630)
+boton4.place(x=345,y=345)
+boton5.place(x=345,y=540)
+boton6.place(x=393,y=235)
+boton7.place(x=230,y=430)
+boton8.place(x=175,y=485)
+boton9.place(x=120,y=430)
+boton10.place(x=175,y=375)
+boton11.place(x=575,y=485)
+boton12.place(x=575,y=375)
+boton13.place(x=630,y=430)
+boton14.place(x=520,y=430)
+boton15.place(x=30,y=640)
+
+#Boton de control
+
+man_auto = BooleanVar() 
+boton_control = Checkbutton(ventana, text='MANUAL',variable=man_auto,onvalue=True, offvalue=False)
+man_auto.set(True)
+boton_control.place(x=70,y=200)
+
+#Etiquetas
+
+Lbl_porcentage_bat = Label(text="%", font = ("Italic", 10, "bold"), bg="white").place (x=710, y= 120)
+
+nivelbateria = StringVar()
+
+Lbl_bat= Label(ventana, textvariable= nivelbateria).place(x=670,y=120)
+
+
+
+def main():
+
+    global oldz
+    global oldy
+    global oldx
+    global counter
+    
+
+
+    while 1:
+
+
+        ventana.update_idletasks()
+        ventana.update()
+
+        
+
+        if counter==10:
+
+            cargabateria()
+            couter=0
+        
+        else:
+
+            counter+=1
+        
+        if man_auto.get()==False:
+    
+            
+
+            jsonResult=arduino.readline()
+            
+
+            try:
+                    
+
+                jsonObject=simplejson.loads(jsonResult)
+                x,y,z,sys,gyro,accel,mag= jsonObject["x"], jsonObject["y"], jsonObject["z"],jsonObject["sys"],jsonObject["gyro"],jsonObject["accel"],jsonObject["mag"]
+
+                Z=float(z)*100/180
+                Y=float(y)*100/90
+                X=(((float(x)-0)*(180-(-180)))/(360-0))+(-180)
+
+                restaz = Z - oldz
+                restay = Y - oldy
+                restax = X - oldx
+
+                
+
+
+                print(Z)
+
+                if Z>10:
+
+                    mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=0.05)
+                
+                elif Z<-10 :
+
+                    mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=0.05)
+
+                else:
+
+                    mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=0, duration=0)
+
+                oldz = Z
+                oldy = Y
+                oldx = X
+
+
+
+            
+            except Exception as Error:
+
+                print(repr(Error))
+
+                pass
+        
+        else:
+
+            ventana.update()
+                
+        
+        
+
+
+if (__name__ == '__main__'):
+	main()
